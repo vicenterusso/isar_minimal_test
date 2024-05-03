b80327ccdf76476d20b1804ff4a5966df0862f7a
Vicente Russo <vicente.russo@gmail.com>
2024-05-02 21:14:47 -0300

    rm cached

--- android/app/src/main/kotlin/com/example/isar_minimal_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.isar_minimal_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- fbuilder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-fvm flutter packages pub get && fvm flutter pub run build_runner build --delete-conflicting-outputs
